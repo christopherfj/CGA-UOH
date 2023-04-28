ad999486dcece8da2a13402bf99732d233d3908d
Christopher Flores Jara, PhD <49974732+christopherfj@users.noreply.github.com>
2023-04-27 20:11:53 -0400

    Add files via upload

---
+++ Cap2/parte_5/cizallamiento/main.py
@@ -0,0 +1,78 @@
+import sys
+from PyQt5 import QtGui, QtWidgets, uic
+from PyQt5.QtCore import pyqtSlot, Qt
+from PyQt5 import QtOpenGL
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+
+#Ventana clase OpenGL
+class Viewer3DWidget(QtOpenGL.QGLWidget):
+    def __init__(self, parent=None):
+        QtOpenGL.QGLWidget.__init__(self, parent)
+        self.color = [255,0,0]
+        self.ciz = (0.0, 0.0)
+        self.coords = [(0.0, 0.5), (0.5, 0.5), (0.5, 0.0), (0.0, 0.0)]
+    #Funciones protegidas OpenGL
+    def paintGL(self):
+        glLoadIdentity()
+        glMatrixMode( GL_MODELVIEW )
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glEnable(GL_DEPTH_TEST)
+        self.dibujar()
+        glFlush()
+    def resizeGL(self, widthInPixels, heightInPixels):
+        glViewport(0, 0, widthInPixels, heightInPixels)
+    def initializeGL(self):
+        glClearColor(0.0, 0.0, 0.0, 1.0)
+        glClearDepth(1.0)
+    #Metodos-seleccion
+    def dibujar(self):
+        glColor3fv(self.color)
+        self.triangulo()
+    #Metodos-figuras
+    def triangulo(self):
+        shx,shy = self.ciz
+        glBegin(GL_POLYGON)
+        for x,y in self.coords:
+            glVertex2f( x+y*shx, y+x*shy )
+        glEnd()
+                  
+#Ventana principal
+class Ventana(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = uic.loadUi('main.ui')
+        self.ui.setWindowIcon(QtGui.QIcon('uoh.jpg'))
+        self.ui.setWindowFlags(Qt.WindowCloseButtonHint | Qt.WindowMinimizeButtonHint)
+        self.viewer3D = Viewer3DWidget(self)
+        self.ui.OpenGLLayout.addWidget( self.viewer3D  )
+        self.ui.show()
+        #variables
+        #signals
+        self.ui.original.clicked.connect(self.original)
+        self.ui.cizx.clicked.connect(self.cizx)
+        self.ui.cizy.clicked.connect(self.cizy)
+    #slots
+    @pyqtSlot()
+    def original(self):
+        self.viewer3D.ciz = (0.0, 0.0)
+        self.viewer3D.updateGL() 
+    @pyqtSlot()
+    def cizx(self):
+        shx = 1
+        shy = 0
+        self.viewer3D.ciz = (shx, shy)
+        self.viewer3D.updateGL()
+    @pyqtSlot()
+    def cizy(self):
+        shx = 0
+        shy = 1
+        self.viewer3D.ciz = (shx, shy)
+        self.viewer3D.updateGL()
+        self.viewer3D.updateGL()
+        
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    ventana = Ventana()
+    sys.exit(app.exec_())
